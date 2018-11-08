ef8aa9020e5947fc2098aa25819b4e05476e13d6
Aymane Chaoui <AymaneShadow@users.noreply.github.com>
2018-11-08 19:49:51 +0000

    Modified displayed passwaord for security reasons.

--- Ahli/App_Code/DBConnectionSQLServer.cs
+++ Ahli/App_Code/DBConnectionSQLServer.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 public class DBConnectionSQLServer
 {
-    public string connetionString = "Data Source=AymaneShadow-PC;Initial Catalog=AFPEE;User ID=AymaneShadow;Password=aymane";
+    public string connetionString = "Data Source=AymaneShadow-PC;Initial Catalog=AFPEE;User ID=AymaneShadow;Password=ThisShouldBeAPassword";
     //public string connetionString = "Data Source=SQL6003.site4now.net;Initial Catalog=DB_A3227B_Ahli;User ID=DB_A3227B_Ahli_admin;Password=EfbV4e9kjQbH-b5C";
     public SqlConnection cn;
 
